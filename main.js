c491f91cfccdc24a5f6a756f6e02c2a8c34ac483
Rajkumar <108186068+RKbikram@users.noreply.github.com>
2024-04-24 11:14:22 +0530

    test

---
+++ main.js
@@ -0,0 +1,7 @@
+let input =document.querySelector('#input');
+let searchbtn =document.querySelector('#search');
+searchbtn.addEventListener('click',function(e){
+    e.preventDefault()
+    // alert('hiiii')
+    // 
+})
